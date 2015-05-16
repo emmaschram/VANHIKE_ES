9a25f7c42c77c1499ecba6ebe1c045ec1a725cee
emmaschram <emmaschram@Emmas-MacBook-Pro.local>
2015-05-16 12:41:33 -0700

    Committing

---
+++ js/mine.js
@@ -0,0 +1,14 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
+
+
+$(document.ready(
+    function(){
+      $("#homelogo").click(
+          function(){
+             console.log("WORKSS");
+          }
+      );
+        
+    }   
+);
